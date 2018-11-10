f9b4c498c6e58836a600a595fa5442b14a8829ff
Hiep Doan <hiep.dv179@gmail.com>
2018-11-10 16:01:26 +0100

    🎉 initialize the project

---
+++ src/main/java/me/hiepdoan/jrqueue/DemoJobQueueSpringRedisApplication.java
@@ -0,0 +1,12 @@
+package me.hiepdoan.jrqueue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoJobQueueSpringRedisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoJobQueueSpringRedisApplication.class, args);
+    }
+}
