d7793b1b2cd24b54ac084773bbd0012f235b2da7
Hiep Doan <hiep.dv179@gmail.com>
2018-11-10 17:21:00 +0100

    🚨add checkstyle and spotbug gradle plugin

--- src/main/java/me/hiepdoan/jrqueue/DemoJobQueueSpringRedisApplication.java
+++ src/main/java/me/hiepdoan/jrqueue/DemoJobQueueSpringRedisApplication.java
@@ -5,8 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoJobQueueSpringRedisApplication {
-
-    public static void main(String[] args) {
-        SpringApplication.run(DemoJobQueueSpringRedisApplication.class, args);
-    }
+  public static void main(String[] args) {
+    SpringApplication.run(DemoJobQueueSpringRedisApplication.class, args);
+  }
 }
